476cab6ffc3021f45847417c6821f7937710cda6
arata.nishijima <cedar.nishi@gmail.com>
2021-12-02 09:20:40 +0000

    first commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,26 @@
+def fizzbuzz(num)
+  # 内容は省略。自力で考えてみましょう。
+    if num % 15 == 0
+        return "FizzBuzz"
+    elsif num % 3 == 0
+        return "Fizz"
+    elsif num % 5 == 0
+        return "Buzz"
+    else 
+        return num
+    end
+end
+
+num_max = 100
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
+#(1..num_max).each do |num|
+#  fizzbuzz(num)
+#end
+
+# 以下は動作確認用の記述です
+#puts fizzbuzz(1)
+#puts fizzbuzz(3)
+#puts fizzbuzz(5)
+#puts fizzbuzz(15)
